70861ba4c2b4757307c26f65ef54f3e0579d8e94
devhaute <devhaute@gmail.com>
2024-04-12 19:13:29 +0900

    chore: 초기 환경 세팅

---
+++ CleanCrypto/CleanCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanCryptoApp.swift
+//  CleanCrypto
+//
+//  Created by kai on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CleanCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
